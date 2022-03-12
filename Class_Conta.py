8222eb91d0c790c6e9705456fba264e899daf486
Gabriel Wagner Piazenski <100728241+gabrielwp87@users.noreply.github.com>
2022-03-11 21:56:58 -0300

    Update Class_Conta.py

--- Class_Conta.py
+++ Class_Conta.py
@@ -1,18 +1,91 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 #self é a referência do endereçamento da memória na qual foi alocada a classe.
 #__init__ é a função construtura.
 
     def extrato(self):
-        print("Saldo de R$ {} do titular {}.".format(self.saldo, self.titular))
+        print("Saldo de R$ {} do titular {}.".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
 
     def saca(self, valor):
-        self.saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor R$ {} passou o limite disponível.".format(valor))
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    #Existe a forma abaixo de usar getters e setters.
+    #Aqui se chama o método da seguinte maneira -> conta.get_saldo(); conta.get_titular; conta.get_limite(); conta.set_limite(limite).
+
+
+    #def get_saldo(self):
+    #    return self.__saldo
+
+    #def get_titular(self):
+    #    return self.__titular
+
+    #def get_limite(self):
+    #    return self.__limite
+
+    #def set_limite(self, limite):
+    #    self.__limite = limite
+
+    #E também existe a forma abaixo de usar getters (@property) e setters (@nome-do-método.setter).
+    #Aqui se chama o método da seguinte forma -> conta.saldo; conta.titular; conta.limite; conta.limite = "valor".
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+
+#método estático é da classe e não do objeto, assim ele pode ser acessado/chamado mesmo sem a criação do objeto. E não manipula informações/atributos de objetos.
+#para ilustrar: no console -> >>> from conta import Conta
+                              #>>> Conta.codigo_banco()
+                              #'001'
+
+                              #>>> from conta import Conta
+                              #>>> codigos = Conta.codigos_bancos()
+                              #>>> codigos
+                              #{'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
+                              #>>> codigos['BB']
+                              #'001'
+                              #>>> codigos['Caixa']
+                              #'104'
+
+                              #Conta.codigos_bancos()['BB']
+                              #'001'
+
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
